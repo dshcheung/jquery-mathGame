cad289ba2f3e8d34b11ffb4e353fd21249603829
Denis Cheung <d.s.h.cheung@gmail.com>
2014-12-02 16:03:14 +0800

    changed structure to block stack on each other

--- js/application.js
+++ js/application.js
@@ -1,5 +1,25 @@
-  $(function() {
-    $("#c-s-slider").slider({
+//Timer Stuff as Global Variable C
+var timeLeft = 10;
+var setTimer;
+var startInterval = function() {
+  setTimer = setInterval(function() {
+    if (timeLeft <= 0) {
+      stopInterval();
+    }
+    $('.rt-timer').html(timeLeft);
+    timeLeft--;
+  }, 1000);
+};
+var stopInterval = function() {
+  clearInterval(setTimer);
+};
+//Timer Stuff as Global Variable C 
+
+//Document Ready S
+$(document).ready(
+  function() {
+    //slider stuff
+    $("#setting-slider").slider({
       range: "min",
       value: 100,
       min: 10,
@@ -8,5 +28,14 @@
         $("#amount").val(ui.value);
       }
     });
-    $("#amount").val($("#c-s-slider").slider("value"));
-  });
+    $("#amount").val($("#setting-slider").slider("value"));
+    
+    //Start Button
+    $(".answer-start-button").on("click", function(){
+      startInterval();
+    });
+
+
+
+  }
+);
